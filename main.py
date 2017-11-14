24093a5731783f3b7e0418ac6874968f627d7b25
Mateusz <mateusz.kupper@gmail.com>
2017-11-14 19:17:01 +0000

    Accuracy

--- main.py
+++ main.py
@@ -1,77 +1,31 @@
 import tensorflow as tf
 import numpy as np
-import glove as glove  
+import util as util 
 import re
-import string 
+import string
 
 glove_dimensionality = 50
-largest_num_of_sentences, largest_num_of_words, largest_num_of_words_any_paragraph = glove.count_words_paragraphs_in_squad()
-largest_num_of_words_in_answer = glove.get_largest_num_of_words_in_answer()
-
-def get_words(classification, answer_num, paragraph_question_mapping):
-	answer_lookup_dict = glove.get_answer_dictionary(answer_num, paragraphs_str, paragraph_question_mapping, largest_num_of_words_any_paragraph+1)
-	vectors = [0 for i in range(largest_num_of_words_in_answer)]
-	i=0
-	for word in classification:
-		j=0
-		emb_max = 0
-		for emb in word:
-			if emb > emb_max:
-				emb_max = emb
-				vector = j	
-			j = j + 1
-		vectors[i] = vector
-		i = i + 1
-
-	ans = ""	
-	for vector in vectors:
-		for word, word_embedding in answer_lookup_dict.iteritems():
-			if word_embedding[vector] == 1:
-				try:
-					ans = ans + " " + word
-				except Exception:
-					ans = ans + " " + str(word)
-				break
-	ans = ans + "."
-	return ans[1:].capitalize()
+largest_num_of_sentences, largest_num_of_words, largest_num_of_words_any_paragraph = util.count_words_paragraphs_in_squad()
+largest_num_of_words_in_answer = util.get_largest_num_of_words_in_answer()
 
 d = 400
 global_step = tf.Variable(0, name="global_step")
 question = tf.placeholder(tf.float32, shape=(largest_num_of_words, glove_dimensionality), name="question")
 text = tf.placeholder(tf.float32, shape=(largest_num_of_sentences, largest_num_of_words, glove_dimensionality), name="text")
-answer = tf.placeholder(tf.int32, shape=(largest_num_of_words_in_answer, largest_num_of_words_any_paragraph+1))
+answer = tf.placeholder(tf.int32, shape=(largest_num_of_words_in_answer, largest_num_of_words_any_paragraph+2))
 
-A = tf.Variable(tf.random_normal([4, glove_dimensionality, d], stddev=0.1), name="A")
-B = tf.Variable(tf.random_normal([4, glove_dimensionality, d], stddev=0.1), name="B")
+A = tf.Variable(tf.random_normal([largest_num_of_sentences, glove_dimensionality, d], stddev=0.1), name="A")
+B = tf.Variable(tf.random_normal([largest_num_of_sentences, glove_dimensionality, d], stddev=0.1), name="B")
 B1 = tf.Variable(tf.random_normal([glove_dimensionality, d], stddev=0.1), name="B1")
-C = tf.Variable(tf.random_normal([4, glove_dimensionality, d], stddev=0.1), name="C")
-W = tf.Variable(tf.random_normal([d,largest_num_of_words_any_paragraph+1], stddev=0.1), name="W")
+C = tf.Variable(tf.random_normal([largest_num_of_sentences, glove_dimensionality, d], stddev=0.1), name="C")
+W = tf.Variable(tf.random_normal([d,largest_num_of_words_any_paragraph+2], stddev=0.1), name="W")
 X = tf.Variable(tf.random_normal([largest_num_of_words_in_answer, largest_num_of_words], stddev=0.1), name="X")
 
-text_unstacked = tf.unstack(text)
-#key addressing
-sentence_question_score = np.zeros((len(text_unstacked)))
-index = 0
-for sentence in text_unstacked:
-	sentence_unstacked = tf.unstack(sentence)
-	for word in sentence_unstacked:
-		question_unstacked = tf.unstack(question)
-		for keyword in question_unstacked:
-			if keyword==word:
-				sentence_question_score[index] = sentence_question_score[index] + 1
-	index = index + 1
-
-#four highest scored sentences
-score_sorted = sentence_question_score.argsort()[-4:][::-1]
-keys = [0 for i in range(4)]
-for a in range(len(keys)):
-	keys[a] = text_unstacked[score_sorted[a]]
-
-M = tf.matmul(keys, A)
-C_m = tf.matmul(keys, C)		
+M = tf.matmul(text, A)
+C_m = tf.matmul(text, C)		
 u = tf.matmul(question, B1)
-for i in range(5):
-	U = tf.stack([u]*4)
+for i in range(3):
+	U = tf.stack([u]*largest_num_of_sentences)
 	sentence_question_match = tf.matmul(M, U, transpose_b=True)
 	P = tf.nn.softmax(sentence_question_match)
 	PC = tf.matmul(P, C_m)
@@ -83,16 +37,22 @@ answer_softmax = tf.nn.softmax(logits=answer_hat, name="answer")
 xentropy = tf.nn.softmax_cross_entropy_with_logits(logits=answer_hat, labels=answer)
 answer_c = tf.cast(answer, tf.float32)
 loss = tf.reduce_mean(xentropy)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0005)
-train = optimizer.minimize(loss)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.003)
+
+#https://stackoverflow.com/questions/36498127/how-to-effectively-apply-gradient-clipping-in-tensor-flow
+gradients, variables = zip(*optimizer.compute_gradients(loss))
+gradients, _ = tf.clip_by_global_norm(gradients, 30.0)
+train = optimizer.apply_gradients(zip(gradients, variables))
+
+#train = optimizer.minimize(loss)
 accuracy = tf.reduce_mean(tf.cast(answer_softmax, tf.float32))
 init = tf.global_variables_initializer()
 
-answers, paragraphs, questions, paragraph_question_mapping = glove.vectorise_squad()
-a, paragraphs_str, c, paragraph_question_mapping = glove.read_squad()
+answers, paragraphs, questions, paragraph_question_mapping = util.vectorise_squad()
+_, paragraphs_str, _, _ = util.read_squad()
 with tf.Session() as sess:
 	init.run()
-	for i in range(200):
+	for i in range(10):
 		answer_num = 0
 		for question_x in questions:	
 			an = answers[answer_num]
@@ -100,17 +60,50 @@ with tf.Session() as sess:
 			pa = paragraphs[paragraph_question_mapping[answer_num]]
 			sess.run(train, feed_dict={question: qu, answer: an, text: pa})
 			acc_train = loss.eval(feed_dict={question: qu, answer: an, text: pa})
+			#if(answer_num%100==0):
 			print(i ,answer_num, " Train accuracy: ", acc_train)
-
+			#if(answer_num%1000==0):
+				#builder = tf.saved_model.builder.SavedModelBuilder("model" + str(i))
+				#builder.add_meta_graph_and_variables(sess, ["tag"], signature_def_map= {
+				#	"model": tf.saved_model.signature_def_utils.predict_signature_def(
+				#	    inputs= {"question": question, "text": text},
+				#	    outputs= {"answer": answer_softmax})
+				#	})
+				#builder.save()
 			feed_dict = {question: qu, text: pa}
 			classification = sess.run(answer_softmax, feed_dict)
-			print get_words(classification, answer_num, paragraph_question_mapping)
+			answer_lookup_dict = util.get_answer_dictionary(answer_num, paragraphs_str, paragraph_question_mapping, largest_num_of_words_any_paragraph+1)
+			print util.get_words(classification, answer_lookup_dict, largest_num_of_words_in_answer)
 			answer_num = answer_num + 1
 
-	builder = tf.saved_model.builder.SavedModelBuilder("model1")
-	builder.add_meta_graph_and_variables(sess, ["tag"], signature_def_map= {
-		"model": tf.saved_model.signature_def_utils.predict_signature_def(
-		    inputs= {"question": question, "text": text},
-		    outputs= {"answer": answer_softmax})
-		})
-	builder.save()
+	#validate
+	answers, paragraphs, questions, new_paragraph_question_mapping = util.vectorise_validation_squad()
+	_, paragraphs_str, _, paragraph_question_mapping = util.read_squad()
+	answer_num = 0
+	absolute_answer_num = 77000
+	overall_accuracy = 0
+	for question_x in questions:	
+		an = answers[answer_num]
+		qu = question_x
+		pa = paragraphs[new_paragraph_question_mapping[answer_num]]
+		feed_dict = {question: qu, text: pa}
+		classification = sess.run(answer_softmax, feed_dict)
+		answer_lookup_dict = util.get_answer_dictionary(absolute_answer_num, paragraphs_str, paragraph_question_mapping, largest_num_of_words_any_paragraph+1)
+		words_answer_hat = util.get_words(classification, answer_lookup_dict, largest_num_of_words_in_answer)
+		words_answer = util.get_words(an, answer_lookup_dict, largest_num_of_words_in_answer)
+		print words_answer_hat
+		print words_answer
+		split_hat = words_answer_hat.split(' ')
+		split = words_answer.split(' ')
+		correct_words_count = 0
+		for word in split:
+			for word_hat in split_hat:
+				if word_hat==word:
+					correct_words_count = correct_words_count + 1
+		accuracy = float(correct_words_count)/float(len(split))
+		print ("Example accuracy: ", accuracy)
+		overall_accuracy = (accuracy+float(overall_accuracy))/float(answer_num + 1)
+
+		print ("Overall accuracy: ", overall_accuracy)
+		answer_num = answer_num + 1	
+		absolute_answer_num = absolute_answer_num + 1
--- predict.py
+++ predict.py
@@ -2,171 +2,157 @@ import os
 import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import numpy as np
-import glove as glove  
+import util as util  
 import re
 import string 
-largest_num_of_sentences, largest_num_of_words, largest_num_of_words_any_paragraph = glove.count_words_paragraphs_in_squad()
-largest_num_of_words_in_answer = glove.get_largest_num_of_words_in_answer()
+largest_num_of_sentences, largest_num_of_words, largest_num_of_words_any_paragraph = util.count_words_paragraphs_in_squad()
+largest_num_of_words_in_answer = util.get_largest_num_of_words_in_answer()
 
-def get_words(classification):
-	vectors = [0 for i in range(largest_num_of_words_in_answer)]
-	i=0
-	for word in classification:
-		j=0
-		emb_max = 0
-		for emb in word:
-			if emb > emb_max:
-				emb_max = emb
-				vector = j	
-			j = j + 1
-		vectors[i] = vector
-		i = i + 1
+def vectorise_paragraph(par):
+	paragraphs_sentences = np.zeros((largest_num_of_sentences, largest_num_of_words, 50))
+	sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', par)
+	j = 0
+	for sentence in sentences:
+		words = sentence.split(' ')
+		v = 0;
+		for word in words:
+			characters = list(word)
+			if len(characters) > 0:
+				if characters[0] in util.special_chars:
+					glove_embedding = util.get_glove_embedding(characters[0])
+					paragraphs_sentences[j][v]=glove_embedding
+					v=v+1
+					word = word[1:]
+				if characters[len(characters)-1] in util.special_chars:
+					word = word[:-1]
+				word = word.lower()
+				if "'" in word and characters[0] not in "'" and characters[len(characters)-1] not in "'":
+					apostrophe_word = word.split("'")
+					glove_embedding = util.get_glove_embedding(apostrophe_word[0])	
+					paragraphs_sentences[j][v]=glove_embedding
+					v=v+1
+					glove_embedding = util.get_glove_embedding("'" + apostrophe_word[1])	
+					paragraphs_sentences[j][v]=glove_embedding
+					v=v+1
+				else:						
+					glove_embedding = util.get_glove_embedding(word)	
+					paragraphs_sentences[j][v]=glove_embedding
+					v=v+1				
+				if characters[len(characters)-1] in util.special_chars:
+					glove_embedding = util.get_glove_embedding(characters[len(characters)-1])
+					paragraphs_sentences[j][v]=glove_embedding
+					v=v+1								
+		j=j+1
+	return	paragraphs_sentences
 
-	ans = ""	
-	for vector in vectors:
-		for word, word_embedding in answer_lookup_dict.iteritems():
-			if word_embedding[vector] == 1:
-				try:
-					ans = ans + " " + word
-				except Exception:
-					ans = ans + " " + str(word)
-				break
-	ans = ans + "."
-	return ans[1:].capitalize()
-
-par = "I was hired at my current workplace just over a month ago. During the interview process, I found out that someone I knew from university was also interviewing for the position. I'll call him Jim. Jim and I have never been close, but are merely aware of each other. Based solely on technical knowledge and relevant expertise, I believe that Jim would have been the better candidate for the job. In particular, he previously worked at a well known company in a position very similar to this one. I was therefore somewhat surprised when I received the offer."
-par = raw_input("Enter paragraph: ")
-ques = "Why were you surprised?"
-ques = raw_input("Enter question: ")
-paragraphs_sentences = np.zeros((largest_num_of_sentences, largest_num_of_words, 50))
-sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', par)
-j = 0
-for sentence in sentences:
-	words = sentence.split(' ')
+def vectorise_question(ques):
+	questions_words = np.zeros((largest_num_of_words, 50))
+	words = ques.split(' ')
 	v = 0;
 	for word in words:
 		characters = list(word)
 		if len(characters) > 0:
-			if characters[0] in glove.special_chars:
-				glove_embedding = glove.get_glove_embedding(characters[0])
-				paragraphs_sentences[j][v]=glove_embedding
+			if characters[0] in util.special_chars:
+				glove_embedding = util.get_glove_embedding(characters[0])
+				questions_words[v]=glove_embedding
 				v=v+1
 				word = word[1:]
-			if characters[len(characters)-1] in glove.special_chars:
+			if characters[len(characters)-1] in util.special_chars:
 				word = word[:-1]
 			word = word.lower()
 			if "'" in word and characters[0] not in "'" and characters[len(characters)-1] not in "'":
 				apostrophe_word = word.split("'")
-				glove_embedding = glove.get_glove_embedding(apostrophe_word[0])	
-				paragraphs_sentences[j][v]=glove_embedding
+				glove_embedding = util.get_glove_embedding(apostrophe_word[0])	
+				questions_words[v]=glove_embedding
 				v=v+1
-				glove_embedding = glove.get_glove_embedding("'" + apostrophe_word[1])	
-				paragraphs_sentences[j][v]=glove_embedding
+				glove_embedding = util.get_glove_embedding("'" + apostrophe_word[1])	
+				questions_words[v]=glove_embedding
 				v=v+1
-			else:						
-				glove_embedding = glove.get_glove_embedding(word)	
-				paragraphs_sentences[j][v]=glove_embedding
+			else:	
+				glove_embedding = util.get_glove_embedding(word)	
+				questions_words[v]=glove_embedding
 				v=v+1				
-			if characters[len(characters)-1] in glove.special_chars:
-				glove_embedding = glove.get_glove_embedding(characters[len(characters)-1])
-				paragraphs_sentences[j][v]=glove_embedding
-				v=v+1								
-	j=j+1
+			if characters[len(characters)-1] in util.special_chars:
+				glove_embedding = util.get_glove_embedding(characters[len(characters)-1])
+				questions_words[v]=glove_embedding
+				v=v+1
+	return questions_words
+proceed = 'p'
+while proceed in ['q', 'p']:
+	if proceed == 'p':
+		par = raw_input("Enter paragraph: ")
+	ques = raw_input("Enter question: ")
 
-questions_words = np.zeros((largest_num_of_words, 50))
-words = ques.split(' ')
-v = 0;
-for word in words:
-	characters = list(word)
-	if len(characters) > 0:
-		if characters[0] in glove.special_chars:
-			glove_embedding = glove.get_glove_embedding(characters[0])
-			questions_words[v]=glove_embedding
-			v=v+1
-			word = word[1:]
-		if characters[len(characters)-1] in glove.special_chars:
-			word = word[:-1]
-		word = word.lower()
-		if "'" in word and characters[0] not in "'" and characters[len(characters)-1] not in "'":
-			apostrophe_word = word.split("'")
-			glove_embedding = glove.get_glove_embedding(apostrophe_word[0])	
-			questions_words[v]=glove_embedding
-			v=v+1
-			glove_embedding = glove.get_glove_embedding("'" + apostrophe_word[1])	
-			questions_words[v]=glove_embedding
-			v=v+1
-		else:	
-			glove_embedding = glove.get_glove_embedding(word)	
-			questions_words[v]=glove_embedding
-			v=v+1				
-		if characters[len(characters)-1] in glove.special_chars:
-			glove_embedding = glove.get_glove_embedding(characters[len(characters)-1])
-			questions_words[v]=glove_embedding
-			v=v+1					
+	paragraphs_sentences = vectorise_paragraph(par)
+	questions_words = vectorise_question(ques)
+			
+	a, paragraphs_str, c, paragraph_question_mapping = util.read_squad()
 
-paragraph_num = 0
+	with tf.Session(graph=tf.Graph()) as sess:
+		tf.saved_model.loader.load(sess, ["tag"], "model1")
+		graph = tf.get_default_graph()
+		question = graph.get_tensor_by_name("question:0")
+		text = graph.get_tensor_by_name("text:0")
+		answer_softmax = graph.get_tensor_by_name("answer:0")
 
-a, paragraphs_str, c, paragraph_question_mapping = glove.read_squad()
+		answer_lookup_dict = {}
+		one_hot_index = 0
+		sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', par)
+		while one_hot_index < largest_num_of_words_any_paragraph - 1:
+			for sentence in sentences:
+				words = sentence.split(' ')
+				for word in words:
+					answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]	
+					characters = list(word)
+					if one_hot_index < largest_num_of_words_any_paragraph - 1:
+						if len(characters) > 0:
+							if characters[0] in util.special_chars:
+								if one_hot_index < largest_num_of_words_any_paragraph - 1:
+									answer_one_hot[one_hot_index] = 1
+									answer_lookup_dict[characters[0]] = answer_one_hot
+									one_hot_index=one_hot_index+1
+									answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
+									word = word[1:]
+							if characters[len(characters)-1] in util.special_chars:
+								word = word[:-1]
+							word = word.lower()
+							if "'" in word and characters[0] not in "'" and characters[len(characters)-1] not in "'":
+								apostrophe_word = word.split("'")
+								if one_hot_index < largest_num_of_words_any_paragraph - 1:	
+									answer_one_hot[one_hot_index] = 1
+									answer_lookup_dict[apostrophe_word[0]] = answer_one_hot
+									one_hot_index = one_hot_index + 1
+									answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
+								if one_hot_index < largest_num_of_words_any_paragraph - 1:
+									answer_one_hot[one_hot_index] = 1	
+									answer_lookup_dict[apostrophe_word[1]] = answer_one_hot
+									one_hot_index = one_hot_index + 1
+									answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
+							else:	
+								if one_hot_index < largest_num_of_words_any_paragraph - 1:
+									answer_one_hot[one_hot_index] = 1
+									answer_lookup_dict[word] = answer_one_hot
+									one_hot_index = one_hot_index + 1
+									answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]				
+							if characters[len(characters)-1] in util.special_chars:
+								if one_hot_index < largest_num_of_words_any_paragraph - 1:
+									answer_one_hot[one_hot_index] = 1
+									answer_lookup_dict[len(characters)-1] = answer_one_hot
+									one_hot_index = one_hot_index + 1
+									answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]			
+		answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
+		answer_one_hot[largest_num_of_words_any_paragraph-2] = 1
+		answer_lookup_dict['unk'] = answer_one_hot
+		feed_dict = {question: questions_words, text: paragraphs_sentences}
+		classification = sess.run(answer_softmax, feed_dict)
+		print util.get_words(classification, answer_lookup_dict, largest_num_of_words_in_answer)
 
-with tf.Session(graph=tf.Graph()) as sess:
-	tf.saved_model.loader.load(sess, ["tag"], "model1")
-	graph = tf.get_default_graph()
-	question = graph.get_tensor_by_name("question:0")
-	text = graph.get_tensor_by_name("text:0")
-	answer_softmax = graph.get_tensor_by_name("answer:0")
-
-	answer_lookup_dict = {}
-	one_hot_index = 0
-	sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', par)
-	while one_hot_index < largest_num_of_words_any_paragraph - 1:
-		for sentence in sentences:
-			words = sentence.split(' ')
-			for word in words:
-				answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]	
-				characters = list(word)
-				if one_hot_index < largest_num_of_words_any_paragraph - 1:
-					if len(characters) > 0:
-						if characters[0] in glove.special_chars:
-							if one_hot_index < largest_num_of_words_any_paragraph - 1:
-								answer_one_hot[one_hot_index] = 1
-								answer_lookup_dict[characters[0]] = answer_one_hot
-								one_hot_index=one_hot_index+1
-								answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
-								word = word[1:]
-						if characters[len(characters)-1] in glove.special_chars:
-							word = word[:-1]
-						word = word.lower()
-						if "'" in word and characters[0] not in "'" and characters[len(characters)-1] not in "'":
-							apostrophe_word = word.split("'")
-							if one_hot_index < largest_num_of_words_any_paragraph - 1:	
-								answer_one_hot[one_hot_index] = 1
-								answer_lookup_dict[apostrophe_word[0]] = answer_one_hot
-								one_hot_index = one_hot_index + 1
-								answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
-							if one_hot_index < largest_num_of_words_any_paragraph - 1:
-								answer_one_hot[one_hot_index] = 1	
-								answer_lookup_dict[apostrophe_word[1]] = answer_one_hot
-								one_hot_index = one_hot_index + 1
-								answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
-						else:	
-							if one_hot_index < largest_num_of_words_any_paragraph - 1:
-								answer_one_hot[one_hot_index] = 1
-								answer_lookup_dict[word] = answer_one_hot
-								one_hot_index = one_hot_index + 1
-								answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]				
-						if characters[len(characters)-1] in glove.special_chars:
-							if one_hot_index < largest_num_of_words_any_paragraph - 1:
-								answer_one_hot[one_hot_index] = 1
-								answer_lookup_dict[len(characters)-1] = answer_one_hot
-								one_hot_index = one_hot_index + 1
-								answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]			
-	answer_one_hot = [0 for i in range(largest_num_of_words_any_paragraph)]
-	answer_one_hot[largest_num_of_words_any_paragraph-2] = 1
-	answer_lookup_dict['unk'] = answer_one_hot
-	feed_dict = {question: questions_words, text: paragraphs_sentences}
-	classification = sess.run(answer_softmax, feed_dict)
-	print get_words(classification)
-		
+	while True:
+		proceed = raw_input("Do you want to ask another question (q), enter new paragraph (p) or exit (e): ")
+		if proceed not in ['p', 'q', 'e']:
+			print "Invalid input"
+		else:
+			break
 
 
 
